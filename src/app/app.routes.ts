be00f107a33d644fe6cc33c54b39b5266700b699
sente.quentin <sente.quentin@gmail.com>
2025-03-30 23:27:59 +0200

    create list of players component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'players',
+    loadChildren: () =>
+      import('./players/players.routes').then(m => m.PLAYERS_ROUTES),
+  },
+];
---
+++ src/app/players/players.routes.ts
@@ -0,0 +1,7 @@
+import { ListComponent } from './list/list.component';
+export const PLAYERS_ROUTES = [
+  {
+    path: '',
+    component: ListComponent,
+  },
+];
